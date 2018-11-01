7b38931e51ccfb56f4ad28fdf287558b29452a4f
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2018-11-01 15:28:06 +0700

    Counting of sentences, words and syllables

--- case4.py
+++ case4.py
@@ -1,9 +1,21 @@
 # Liza
 # Создание проекта на github, анализа текста на кол-во предложений, слов и слогов.
+vowel = 'аоиеёэыуюя'
+text = input()
+count = 0
+
+sentences = text.count('.')
+words = text.count(' ') + 1
+for i in range(0, len(text)):
+    if vowel.find(text[i]) != -1:
+        count += 1
+
+print('{} {}'.format('Предложений:', sentences))
+print('{} {}'.format('Слов:', words))
+print('{} {}'.format('Слогов:', count))
 
 # Alina
 # Средняя дина предложения в словах, длина слова в слогах, индекс удобочитаемости Флеша, pep8.
 
 # Aygerim
 # Анализ индекса Флеша и вывод, гu_local.
-
