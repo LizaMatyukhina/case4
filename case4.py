3e56dd1ad716cd4219159fee6338f8759355727c
aigerimka <aigerish0603@gmail.com>
2018-11-05 20:24:04 +0700

    analysis and index output,ru_local file

--- case4.py
+++ case4.py
@@ -10,12 +10,20 @@ for i in range(0, len(text)):
     if vowel.find(text[i]) != -1:
         count += 1
 
-print('{} {}'.format('Предложений:', sentences))
-print('{} {}'.format('Слов:', words))
-print('{} {}'.format('Слогов:', count))
+print('{} {}'.format(COUNT_SENTENS, sentences))
+print('{} {}'.format(COUNT_WORDS, words))
+print('{} {}'.format(COUNT_SYLLABLES, count))
 
 # Alina
 # Средняя дина предложения в словах, длина слова в слогах, индекс удобочитаемости Флеша, pep8.
 
-# Aygerim
+# Aigerim
 # Анализ индекса Флеша и вывод, гu_local.
+if FRE >= 80:
+    print('{}'.format(VERY_EASY_TO_READ))
+elif FRE >= 50:
+    print('{}'.format(EASY_TEXT))
+elif FRE >= 25:
+    print('{}'.format(LITTLE_HARD_TO_READ))
+else:
+    print('{}'.format(HARD_TO_READ))
